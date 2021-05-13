300d0beec16c07ecc2a02de6b80ef4aa90c856a0
franeldev <franel4u@gmail.com>
2021-05-13 07:55:44 +0100

    server.js updates

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const connectDB = require('./db');
+require('dotenv').config(); //allows us to use the environmental variables in the .env
+const { PORT } = process.env
 
 // Connect to db
 connectDB();
@@ -12,4 +14,10 @@ app.use(express.json({ extended: false }));
 
 // Create a basic express route
 app.get('/', (req, res) => res.json({ message: 'CRUD App with Database' }));
+
+// PORT
+const port = process.env.PORT || PORT;
+
+// Listen to connection
+app.listen(port, () => console.log(`server running on port ${port}`));
 
