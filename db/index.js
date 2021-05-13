236efee0c74e712cc7faa156e99e6062cbafe96d
franeldev <franel4u@gmail.com>
2021-05-13 13:18:55 +0100

    local db connection

--- db/index.js
+++ db/index.js
@@ -6,7 +6,25 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const { MONGO_URI } = process.env;
 
-// Create the connection function
+// Async/Await mongoose connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.log(err.message);
+    
+    // Exit with failure
+    process.exit(1);
+  }
+}
+
+// OR Create the connection function using the normal arrow function with .then method
 // const connectDB  = () => {
 //   mongoose.connect(MONGO_URI, {
 //     useNewUrlParser: true,
@@ -24,23 +42,5 @@ const { MONGO_URI } = process.env;
 //     process.exit(1);
 //   })
 // }
-
-// Async mongoose connection
-const connectDB = async () => {
-  try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false
-    });
-    console.log('MongoDB connected...');
-  } catch (err) {
-    console.log(err.message);
-    
-    // Exit with failure
-    process.exit(1);
-  }
-}
 
 module.exports = connectDB;
